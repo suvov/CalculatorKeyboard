a5277435aed158a106e84f153bca8fe107dc5eb2
Vladimir Shutyuk <shookup@gmail.com>
2022-06-17 15:44:22 +0400

    Decimal rounding extension

---
+++ Sources/Calcboard/Decimal+rounded.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension Decimal {
+    func rounded(_ scale: Int, _ roundingMode: NSDecimalNumber.RoundingMode) -> Decimal {
+        var result = Decimal()
+        var localCopy = self
+        NSDecimalRound(&result, &localCopy, scale, roundingMode)
+        return result
+    }
+}
