71bc21d3ef5eacf1f40fa5fb7ec67ba4d7790ead
Vladimir Shutyuk <shookup@gmail.com>
2022-06-17 15:44:47 +0400

    Implement evaluate

--- Sources/Calcboard/Evaluator.swift
+++ Sources/Calcboard/Evaluator.swift
@@ -1,5 +1,43 @@
+import Foundation
+
 struct Evaluator {
+    private let scale: Int
+
+    init(scale: Int = 2) {
+        self.scale = scale
+    }
+
     func evaluate(_ expression: Expression) -> String? {
-        nil
+        switch expression {
+        case .lhsOperatorRhs(let lhs, let opt, let rhs):
+            return evaluate(lhs: lhs, rhs: rhs, opt: opt)
+        default:
+            return nil
+        }
+    }
+}
+
+private extension Evaluator {
+    func evaluate(lhs: String, rhs: String, opt: Operator) -> String? {
+        guard let lhsDecimal = Decimal(string: lhs),
+              let rhsDecimal = Decimal(string: rhs)
+        else {
+            return nil
+        }
+        var result: Decimal?
+        switch opt {
+        case .addition:
+            result = (lhsDecimal + rhsDecimal).rounded(scale, .bankers)
+        case .subtraction:
+            result = (lhsDecimal - rhsDecimal).rounded(scale, .bankers)
+        case .multiplication:
+            result = (lhsDecimal * rhsDecimal).rounded(scale, .bankers)
+        case .division:
+            result = (lhsDecimal / rhsDecimal).rounded(scale, .bankers)
+        }
+        if let result = result, result != Decimal.nan {
+            return result.description
+        }
+        return nil
     }
 }
