ea4af92c0496ee4906e5addee785089f6e82cd81
Vladimir Shutyuk <shookup@gmail.com>
2022-06-17 15:44:00 +0400

    Evaluator tests

---
+++ Tests/CalcboardTests/EvaluatorTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+@testable import Calcboard
+
+final class EvaluatorTests: XCTestCase {
+    
+    private let evaluator = Evaluator(scale: 2)
+    
+    func testDivizionByZero() {
+        let expression = Expression.lhsOperatorRhs("1", .division, "0")
+        XCTAssertNil(evaluator.evaluate(expression))
+    }
+    
+    func testAddition() {
+        let expression = Expression.lhsOperatorRhs("1.22", .addition, "1.88")
+        XCTAssertEqual(evaluator.evaluate(expression), "3.1")
+    }
+    
+    func testSutraction() {
+        let expression = Expression.lhsOperatorRhs("3.11", .subtraction, "2.11")
+        XCTAssertEqual(evaluator.evaluate(expression), "1")
+    }
+    
+    func testDivision() {
+        let expression = Expression.lhsOperatorRhs("3.33", .division, "3")
+        XCTAssertEqual(evaluator.evaluate(expression), "1.11")
+    }
+    
+    func testMultiplication() {
+        let expression = Expression.lhsOperatorRhs("1.11", .multiplication, "3.0")
+        XCTAssertEqual(evaluator.evaluate(expression), "3.33")
+    }
+}
