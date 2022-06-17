1f26985b6a597ebe59b8ef8862dded33dda5c11e
Vladimir Shutyuk <shookup@gmail.com>
2022-06-17 15:51:54 +0400

    Validator tests

---
+++ Tests/CalcboardTests/ValidatorTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+@testable import Calcboard
+
+class ValidatorTests: XCTestCase {
+
+    private let validator = Validator()
+
+    func testValidatesPartialDecimal() {
+        XCTAssertFalse(validator.isValidPartialDecimalString(""))
+
+        XCTAssertFalse(validator.isValidPartialDecimalString("."))
+        XCTAssertFalse(validator.isValidPartialDecimalString("00"))
+        XCTAssertFalse(validator.isValidPartialDecimalString("0.."))
+        XCTAssertFalse(validator.isValidPartialDecimalString("0.111"))
+
+        XCTAssertFalse(validator.isValidPartialDecimalString("-1"))
+
+        XCTAssertTrue(validator.isValidPartialDecimalString("0."))
+        XCTAssertTrue(validator.isValidPartialDecimalString("2."))
+        XCTAssertTrue(validator.isValidPartialDecimalString("99995."))
+        XCTAssertTrue(validator.isValidPartialDecimalString("100."))
+        XCTAssertTrue(validator.isValidPartialDecimalString("111"))
+    }
+
+    func testValidatesDecimal() {
+        XCTAssertFalse(validator.isValidDecimalString(""))
+        XCTAssertFalse(validator.isValidDecimalString("."))
+        XCTAssertFalse(validator.isValidDecimalString("00"))
+        XCTAssertFalse(validator.isValidDecimalString("0.."))
+        XCTAssertFalse(validator.isValidDecimalString("0.111"))
+
+        XCTAssertFalse(validator.isValidPartialDecimalString("-9.99"))
+
+        XCTAssertTrue(validator.isValidDecimalString("0.1"))
+        XCTAssertTrue(validator.isValidDecimalString("2.1"))
+        XCTAssertTrue(validator.isValidDecimalString("99995.32"))
+        XCTAssertTrue(validator.isValidDecimalString("100.99"))
+        XCTAssertTrue(validator.isValidDecimalString("111"))
+    }
+}
