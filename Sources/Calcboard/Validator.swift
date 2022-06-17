fd1ab411b1a98346269501beca8256d1e375a064
Vladimir Shutyuk <shookup@gmail.com>
2022-06-17 15:52:10 +0400

    Implement validator with regex

--- Sources/Calcboard/Validator.swift
+++ Sources/Calcboard/Validator.swift
@@ -1,9 +1,30 @@
-struct Validator {
-    func isValidPartialString(_ string: String) -> Bool {
-        false
-    }
+import Foundation
 
-    func isValidString(_ string: String) -> Bool {
-        false
+struct Validator {
+    func isValidDecimalString(_ string: String) -> Bool {
+        let floatPattern = "^([0]|[(1-9)]+([(0-9)]+)?)([.][0-9]{1,2})?$"
+        return validateString(string, withPattern: floatPattern)
+    }
+    
+    func isValidPartialDecimalString(_ string: String) -> Bool {
+        let partialFloatPattern = "^([0]|([(1-9)]+)([(0-9)]+)?)([.]([0-9]{1,2})?)?$"
+        return validateString(string, withPattern: partialFloatPattern)
+    }
+}
+
+private extension Validator {
+    func validateString(_ string: String, withPattern pattern: String) -> Bool {
+        if string.isEmpty { return false }
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+            let numMatches = regex.numberOfMatches(
+                in: string,
+                options: [],
+                range: NSRange(location: 0, length: string.count)
+            )
+            return numMatches == 1
+        } catch {
+            return false
+        }
     }
 }
