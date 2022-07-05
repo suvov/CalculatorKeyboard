534496eb38ca871c673aa4af2de7b135dd85d5d2
Vladimir Shutyuk <vladimir.shutyuk@osome.com>
2022-07-05 15:25:52 +0400

    Set decimal value in transformer. Update tests

--- Sources/CalculatorKeyboard/Transformer.swift
+++ Sources/CalculatorKeyboard/Transformer.swift
@@ -1,12 +1,19 @@
 import Foundation
 import Combine
 
-final class Transformer {
-    struct Output {
-        let text: String
-        let decimalValue: Decimal?
+extension Transformer {
+    struct Input {
+        let keyboard: AnyPublisher<KeyboardInput, Never>
+        let decimalValue: AnyPublisher<Decimal?, Never>
     }
-    
+
+    struct Output {
+        let text: AnyPublisher<String, Never>
+        let decimalValue: AnyPublisher<Decimal?, Never>
+    }
+}
+
+final class Transformer {
     private let reducer: Reducer
     private let formatter: Formatter
 
@@ -14,16 +21,47 @@ final class Transformer {
         self.reducer = reducer
         self.formatter = formatter
     }
-    
-    func transform(input: AnyPublisher<KeyboardInput, Never>) -> AnyPublisher<Output, Never> {
-        var expression = Expression.empty
-        return input
-            .map { [unowned self] input -> Output in
-                expression = self.reducer.reduce(expression, with: input)
-                let text = self.formatter.string(from: expression)
-                let decimalValue = expression.value
-                return Output(text: text, decimalValue: decimalValue)
+
+    func transform(input: Input) -> Output {
+        var localExpression = Expression.empty
+
+        let expressionFromKeyboardInput = input.keyboard
+            .map { [unowned self] keyboardInput -> Expression in
+                return self.reducer.reduce(localExpression, with: keyboardInput)
             }
             .eraseToAnyPublisher()
+
+        let expressionFromDecimalValueInput = input.decimalValue
+            .map { decimalValue -> Expression in
+                if let decimalValue = decimalValue {
+                    return .lhs(decimalValue.string)
+                } else {
+                    return .empty
+                }
+            }
+
+        let expression = Publishers.Merge(
+            expressionFromKeyboardInput,
+            expressionFromDecimalValueInput
+        ).handleEvents(receiveOutput: {
+            localExpression = $0
+        }).share()
+
+        let text = expression.map { [unowned self] in
+            self.formatter.string(from: $0)
+        }.eraseToAnyPublisher()
+
+        let decimalValue = expression.map {
+            $0.value
+        }.eraseToAnyPublisher()
+
+        return Output(text: text, decimalValue: decimalValue)
+    }
+}
+
+private extension Decimal {
+    var string: String {
+        var value = self
+        return NSDecimalString(&value, Constants.locale)
     }
 }
--- Tests/CalculatorKeyboardTests/TransformerTests.swift
+++ Tests/CalculatorKeyboardTests/TransformerTests.swift
@@ -5,6 +5,7 @@ import XCTest
 class TransformerTests: XCTestCase {
 
     private var subscriptions: Set<AnyCancellable>!
+    private typealias Output = (String, Decimal?)
 
     private let transformer = Transformer(
         reducer: Reducer(validator: Validator(),
@@ -16,50 +17,48 @@ class TransformerTests: XCTestCase {
         super.setUp()
         subscriptions = []
     }
-    
+
     func testLhsCase1() {
         let inputs: [KeyboardInput] = [.digit(.one)]
-        let expectedOutput = Transformer.Output(text: "1", decimalValue: Decimal(string: "1")!)
-        testTransformsInputs(inputs, into: expectedOutput)
+        let expected: Output = (text: "1", decimalValue: Decimal(string: "1")!)
+        testTransformsKeyboardInputs(inputs, into: expected)
     }
-    
+
     func testLhsCase2() {
         let inputs: [KeyboardInput] = [.digit(.one),
                               .decimalSeparator,
                               .digit(.zero),
                               .digit(.one)]
-        let expectedOutput = Transformer.Output(text: "1.01", decimalValue: Decimal(string: "1.01"))
-        testTransformsInputs(inputs, into: expectedOutput)
+        let expected: Output = (text: "1.01", decimalValue: Decimal(string: "1.01"))
+        testTransformsKeyboardInputs(inputs, into: expected)
     }
-    
+
     func testLhsCase3() {
         let inputs: [KeyboardInput] = [.digit(.one),
                               .decimalSeparator,
                               .digit(.one)]
-        let expectedOutput = Transformer.Output(text: "1.1",
-                                              decimalValue: Decimal(string: "1.1"))
-        testTransformsInputs(inputs, into: expectedOutput)
+        let expected: Output = (text: "1.1", decimalValue: Decimal(string: "1.1"))
+        testTransformsKeyboardInputs(inputs, into: expected)
     }
-    
+
     func testLhsCase4() {
         let inputs: [KeyboardInput] = [.digit(.one),
                               .decimalSeparator,
                               .digit(.one),
                               .digit(.zero)]
-        let expectedOutput = Transformer.Output(text: "1.1",
-                                              decimalValue: Decimal(string: "1.1"))
-        testTransformsInputs(inputs, into: expectedOutput)
+        let expected: Output = (text: "1.1", decimalValue: Decimal(string: "1.1"))
+        testTransformsKeyboardInputs(inputs, into: expected)
     }
-    
+
     func testLhsOperator() {
         let inputs: [KeyboardInput] = [.digit(.nine),
                                .decimalSeparator,
                                .digit(.two),
                                .arithmeticOperator(.addition)]
-        let expectedOutput = Transformer.Output(text: "9.2 + ", decimalValue: nil)
-        testTransformsInputs(inputs, into: expectedOutput)
+        let expected: Output = (text: "9.2 + ", decimalValue: nil)
+        testTransformsKeyboardInputs(inputs, into: expected)
     }
-    
+
     func testLhsOperatorRhs() {
         let inputs: [KeyboardInput] = [.digit(.three),
                                .decimalSeparator,
@@ -68,10 +67,10 @@ class TransformerTests: XCTestCase {
                                .digit(.four),
                                .decimalSeparator,
                                .digit(.eight)]
-        let expectedOutput = Transformer.Output(text: "3.2 + 4.8", decimalValue: nil)
-        testTransformsInputs(inputs, into: expectedOutput)
+        let expected: Output = (text: "3.2 + 4.8", decimalValue: nil)
+        testTransformsKeyboardInputs(inputs, into: expected)
     }
-    
+
     func testCalculates() {
         let inputs: [KeyboardInput] = [.digit(.three),
                                .decimalSeparator,
@@ -81,40 +80,119 @@ class TransformerTests: XCTestCase {
                                .decimalSeparator,
                                .digit(.eight),
                                .equals]
-        let expectedOutput = Transformer.Output(text: "8", decimalValue: Decimal(string: "8"))
-        testTransformsInputs(inputs, into: expectedOutput)
+        let expected: Output = (text: "8", decimalValue: Decimal(string: "8"))
+        testTransformsKeyboardInputs(inputs, into: expected)
+    }
+
+    func testSetsDecimalValueEmpty() {
+        let inputs: [KeyboardInput] = []
+        let decimal = Decimal(string: "8")
+        testSetsDecimalValue(keyboardInputs: inputs, decimal: decimal, into: "8")
+    }
+
+    func testSetsDecimalValueLhs() {
+        let inputs: [KeyboardInput] = [.digit(.three),
+                                       .decimalSeparator,
+                                       .digit(.two)]
+        let decimal = Decimal(string: "8")
+        testSetsDecimalValue(keyboardInputs: inputs, decimal: decimal, into: "8")
+    }
+
+    func testSetsDecimalValueLhsOperator() {
+        let inputs: [KeyboardInput] = [.digit(.three),
+                                       .decimalSeparator,
+                                       .digit(.two),
+                                       .arithmeticOperator(.addition)]
+        let decimal = Decimal(string: "8")
+        testSetsDecimalValue(keyboardInputs: inputs, decimal: decimal, into:  "8")
+    }
+
+    func testSetsDecimalValueLhsOperatorRhs() {
+        let inputs: [KeyboardInput] = [.digit(.three),
+                                       .decimalSeparator,
+                                       .digit(.two),
+                                       .arithmeticOperator(.addition),
+                                       .digit(.four),
+                                       .decimalSeparator,
+                                       .digit(.eight)]
+        let decimal = Decimal(string: "8")
+        testSetsDecimalValue(keyboardInputs: inputs, decimal: decimal, into: "8")
+    }
+
+    func testSetsDecimalValueNil() {
+        let inputs: [KeyboardInput] = [.digit(.three),
+                                       .decimalSeparator,
+                                       .digit(.two)]
+        testSetsDecimalValue(keyboardInputs: inputs, decimal: nil, into: "")
     }
 }
 
 private extension TransformerTests {
-    func testTransformsInputs(_ inputs: [KeyboardInput], into expectedOutput: Transformer.Output) {
-        let inputSubject = PassthroughSubject<KeyboardInput, Never>()
-        
-        let output = transformer.transform(input: inputSubject.eraseToAnyPublisher())
-        
-        var received: Transformer.Output?
-        let expectation = self.expectation(description: "Output")
-        var count = inputs.count
-        output.sink {
-            received = $0
-            count -= 1
-            if count == 0 {
-                expectation.fulfill()
-            }
-        }
-        .store(in: &subscriptions)
-        
-        for input in inputs {
-            inputSubject.send(input)
-        }
-        
-        waitForExpectations(timeout: 1)
-        XCTAssertEqual(received, expectedOutput)
-    }
-}
+    func testTransformsKeyboardInputs(_ keyboardInputs: [KeyboardInput],
+                                      into expected: (String, Decimal?)) {
+        let keyboardSubject = PassthroughSubject<KeyboardInput, Never>()
+        let decimalSubject = Empty<Decimal?, Never>()
 
-extension Transformer.Output: Equatable {
-    public static func == (lhs: Transformer.Output, rhs: Transformer.Output) -> Bool {
-        lhs.text == rhs.text && lhs.decimalValue == rhs.decimalValue
+        let transformerInput = Transformer.Input(
+            keyboard: keyboardSubject.eraseToAnyPublisher(),
+            decimalValue: decimalSubject.eraseToAnyPublisher()
+        )
+        let output = transformer.transform(input: transformerInput)
+
+        var received: Output?
+        let expectation = self.expectation(description: "Output")
+        var count = keyboardInputs.count
+
+        Publishers.CombineLatest(output.text, output.decimalValue)
+            .sink {
+                received = $0
+                count -= 1
+                if count == 0 {
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &subscriptions)
+        for input in keyboardInputs {
+            keyboardSubject.send(input)
+        }
+
+        waitForExpectations(timeout: 1)
+        XCTAssertEqual(received?.0, expected.0)
+        XCTAssertEqual(received?.1, expected.1)
+    }
+
+    func testSetsDecimalValue(keyboardInputs: [KeyboardInput],
+                              decimal: Decimal?,
+                              into expected: String) {
+        let keyboardSubject = PassthroughSubject<KeyboardInput, Never>()
+        let decimalSubject = PassthroughSubject<Decimal?, Never>()
+
+        let transformerInput = Transformer.Input(
+            keyboard:  keyboardSubject.eraseToAnyPublisher(),
+            decimalValue: decimalSubject.eraseToAnyPublisher()
+        )
+        let output = transformer.transform(input: transformerInput)
+
+        var received: String?
+        let expectation = self.expectation(description: "Output")
+        var count = keyboardInputs.count + 1 // inputs + decimal
+
+        output.text
+            .sink {
+                received = $0
+                count -= 1
+                if count == 0 {
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &subscriptions)
+
+        for input in keyboardInputs {
+            keyboardSubject.send(input)
+        }
+        decimalSubject.send(decimal)
+
+        waitForExpectations(timeout: 1)
+        XCTAssertEqual(received, expected)
     }
 }
